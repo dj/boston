2bccb9d46d9fe0e087aad953973732f6728f0461
DJ Hartman <danieljayhartman@gmail.com>
2015-04-09 17:29:47 -0400

    Load data.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
+var canvas = $('.top-tracts'),
+    canvasContainer = $('.top-tracts-container');
+
+// Set the canvas width to the width of it's container
+canvas.width( canvasContainer.width());
+
+// Load the data
+d3.csv('data/boston-census-2010.csv', parse, loaded);
+
+function parse(row) {
+  return row;
+}
+
+function loaded(err, data) {
+  console.log(data);
+}
 
